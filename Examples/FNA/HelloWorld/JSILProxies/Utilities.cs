b077d9dc1664952c8301a46f2d625f8e0c98f43c
Jaiden Mispy <jaiden@mispy.me>
2015-01-20 19:43:56 +1100

    Add WIP FNA test example w/ proxy

---
+++ Examples/FNA/HelloWorld/JSILProxies/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using JSIL.Meta;
+using JSIL.Proxy;
+
+namespace HelloWorld.JSILProxies {
+    [JSProxy(
+        "MonoGame.Utilities.AssemblyHelper",
+        JSProxyMemberPolicy.ReplaceDeclared,
+        JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceDeclared
+    )]
+    public abstract class AssemblyHelperProxy {
+        [JSReplacement("document.title")]
+        public static string GetDefaultWindowTitle() {
+            throw new NotImplementedException();
+        }
+    }
+}
